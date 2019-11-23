099a3180669b8b32f7743ea3dde709ecd4bcd0dc
Jebnai <47109582+Jebnai@users.noreply.github.com>
2019-11-23 00:32:44 +0000

    Bit more efficient but still be running like twitch chat slow mode

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@
 
 
 //works out if a candidate has majority of the first preferences.
-bool majority(election &e){
+bool majority(election &e, std::vector<std::pair<unsigned int, int>> &rank){
     int sum = 0;
-    for(int i = 1; i < e.ranked_candidates().size(); i++){
-        sum += e.ranked_candidates()[i].second;
+    for(int i = 1; i < rank.size(); i++){
+        sum += rank[i].second;
     }
-    return(e.ranked_candidates()[0].second > sum);
+    return(rank[0].second > sum);
 }
 
 int main() {
@@ -23,7 +23,7 @@ int main() {
     int counter = 1;
     //reading from text file
     std::fstream in;
-    in.open("tests/input4.txt");
+    in.open("votes.txt");
     if(in.fail()){
         std::cerr << "Error reading file" << std::endl;
         exit(1);
@@ -31,28 +31,28 @@ int main() {
         election = read_votes(in);
     }
     in.close();
-
+    std::vector<std::pair<unsigned int, int>> rank;
     //running the election
     while(election.ranked_candidates().size() > 1 ){
+        rank = election.ranked_candidates();
         std::cout << "Round " << counter << ": " << election.vote_count() << " votes" << std::endl;
         std::cout << "First preferences:" << std::endl;
-        for(int i = 0; i < election.ranked_candidates().size(); i++){
-            std::cout << "   Candidate " << election.ranked_candidates()[i].first << ": " << election.ranked_candidates()[i].second << std::endl;
+        for(int i = 0; i < rank.size(); i++){
+            std::cout << "   Candidate " << rank[i].first << ": " << rank[i].second << std::endl;
         }
         //check to see if a candidate has majority of the votes
-        if(majority(election)){
-            for(int i = 1; i < election.ranked_candidates().size(); i++){
-                election.eliminate(election.ranked_candidates()[i].first);
-                std::cout << "Candidate " << election.ranked_candidates()[0].first << " is selected." << std::endl;
+        if(majority(election, rank)){
+            for(int i = 1; i < rank.size(); i++){
+                election.eliminate(rank[i].first);
+                std::cout << "Candidate " << rank[0].first << " is selected." << std::endl;
                 break;
             }
             break;
         }else{
-            std::cout << "Candidate " << election.ranked_candidates()[election.ranked_candidates().size()-1].first << " is eliminated" << std::endl;
-            election.eliminate(election.ranked_candidates()[election.ranked_candidates().size()-1].first);
+            std::cout << "Candidate " << rank[rank.size()-1].first << " is eliminated" << std::endl;
+            election.eliminate(rank[rank.size()-1].first);
         }counter++;
     }
 
     return 0;
 }
-
